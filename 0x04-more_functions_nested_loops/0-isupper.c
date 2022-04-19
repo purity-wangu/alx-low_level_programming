6e1f7969bb1caf7e6981074e022776c914ebfeaf
purity-wangu <wanguwanduati@gmail.com>
2022-04-20 00:49:33 +0300

    More Nested Loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "purity.wangu"
+
+/**
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
