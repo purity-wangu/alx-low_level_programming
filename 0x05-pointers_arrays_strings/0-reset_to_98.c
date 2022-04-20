f1ec3e02a40470e1be19411dcbe949c6a7a08030
purity-wangu <wanguwanduati@gmail.com>
2022-04-20 14:33:55 +0300

    battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
-#include "stdio.h"
+#include <stdio.h>
 /**
  * @n : n is the pointer of n and return value 98
  * Return: Always 0.
  */
-void reset_to_98(int *n)
+
+void reset_to_98(int *n);
 {
 	*n = 98;
 }
