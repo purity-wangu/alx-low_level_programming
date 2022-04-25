9108d217747f4842eb99c9d54f4420c48bffbe2d
purity-wangu <wanguwanduati@gmail.com>
2022-04-25 23:43:01 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 
-	_putchar(72);
-	_putchar(111);
-	_putchar(108);
-	_putchar(98);
-	_putchar(101);
-	_putchar(114);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
 	_putchar(116);
-	_putchar(111);
-	_putchar(110);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar ('\n');
 	return (0);
 }
