9367f54115cf3af16d0e07ff7c5b5450c875c822
purity-wangu <wanguwanduati@gmail.com>
2022-04-25 23:48:14 +0300

    advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,55 @@
 #include <stdio.h>
 
 /**
- * natural - Write a program that computes and prints the sum of
- * all the multiples of 3 or 5 below 1024 (excluded)
- * @i: Integer
- * @r: Result
- * Return: On success 1.
- *
+ * fibonacci - caleld by main()
+ * Return: without return
  */
+void fibonacci(void)
+{
+	int i, val;
+	unsigned long int num1 = 0, num1Tail, num2 = 1, num2Tail;
+	unsigned long int tmpResult, tmpResultTail;
 
+	for (i = 1; i <= 92; i++)
+	{
+		tmpResult = num1 + num2;
+		printf("%lu", tmpResult);
+		putchar(',');
+		putchar(' ');
+		num1 = num2;
+		num2 = tmpResult;
+	}
+	num1Tail = num1 % 1000000000;
+	num1 = num1 / 1000000000;
+	num2Tail = num2 % 1000000000;
+	num2 = num2 / 1000000000;
+	for (i = 93; i <= 98; i++)
+	{
+		tmpResult = num1 + num2;
+		tmpResultTail = num1Tail + num2Tail;
+		val = tmpResultTail / 1000000000;
+		if (val)
+		{
+			tmpResult = tmpResult + val;
+			tmpResultTail = tmpResultTail % 1000000000;
+		}
+		printf("%lu%lu", tmpResult, tmpResultTail);
+		num1 = num2;
+		num1Tail = num2Tail;
+		num2 = tmpResult;
+		num2Tail = tmpResultTail;
+		if (i < 98)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - Initial function
+ * Return: return 0 to success
+ */
 int main(void)
 {
-	int i;
-	float a, b, r;
-	a = 1;
-	b = 2;
-	r= 0;
-
-	printf("1, 2");
-	for(i = 1; i <= 96; i++)
-	{
-		r = a + b;
-		a = b;
-		b = r;
-		printf(", %.0f",r);
-	}
-	putchar('\n');
+	fibonacci();
 	return (0);
 }
