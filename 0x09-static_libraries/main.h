9a2d5b89e6a060fd4219acd908e15baa1fa61dce
purity-wangu <wanguwanduati@gmail.com>
2022-05-03 23:17:07 +0300

    correction

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,7 @@
-#ifndef Purity_Wangu
-#define Purity_Wangu
+#ifndef PURITY_WANGU
+#define PURITY_WANGU
 
+#include <stddef.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,5 +24,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* PURITY_WANGU */
 
