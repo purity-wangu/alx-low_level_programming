9a2426a5a47cf78da87e809c73fc6061e0055d4b
purity-wangu <wanguwanduati@gmail.com>
2022-05-12 14:50:04 +0300

    preprocessor and macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+
+/**
+ * main - Initial function
+ * Return: return 0 to success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef sum_two_numbers
+#define sum_two_numbers
+#define SUM(x, y) (x + y)
+#endif
