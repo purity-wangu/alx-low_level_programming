e35d88325073cf859799b5702f5335d1396ec23d
purity-wangu <wanguwanduati@gmail.com>
2022-05-15 21:27:28 +0300

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,56 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - created new array using malloc().
- * @s1: chain of chars one.
- * @s2: chain of chars two.
- * @n : number of chars to use of s2.
- * Return: new pointer with s1 and s2 added.
+ * *string_nconcat - Concatinates two strings
+ *
+ * @s1: char pointer
+ *
+ * @s2: char pointer
+ *
+ * @n: unsigned int
+ *
+ * Return: Pointer to s1, n bytes of s2
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0;
-	unsigned int j = 0;
-	char *stringMalloc = "";
+	unsigned int counter1, counter2, i, j;
+	char *strTemp;
 
-	if (s1)
-		while (s1[i])
-		{
-			i++;
-		}
-	if (s2)
-		while (s2[j] && j < n)
-		{
-			j++;
-		}
+	counter1 = 0;
+	counter2 = 0;
 
-	i += j;
-	stringMalloc = malloc(sizeof(char) * i + 1);
-	if (stringMalloc == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[counter1] != '\0')
+		counter1++;
+	while (s2[counter2] != '\0')
+		counter2++;
+
+	if (n > counter2)
+	{
+		n = counter2;
+	}
+	strTemp = malloc(sizeof(char) * (counter1 + (n + 1)));
+
+	if (strTemp == NULL)
 	{
 		return (NULL);
 	}
-	i = 0;
-	if (s1)
-		while (s1[i])
-		{
-			stringMalloc[i] = s1[i];
-			i++;
-		}
-	j = 0;
-	if (s2)
-		while (s2[j] && j < n)
-		{
-			stringMalloc[i] = s2[j];
-			i++;
-			j++;
-		}
-	stringMalloc[i] = '\0';
+	for (i = 0; i < counter1; i++)
+	{
+		strTemp[i] =  s1[i];
+	}
 
-	return (stringMalloc);
+	for (j = 0; j < n; j++)
+	{
+		strTemp[i] = s2[j];
+		i++;
+	}
+	strTemp[i] = '\0';
+	return (strTemp);
 }
+
